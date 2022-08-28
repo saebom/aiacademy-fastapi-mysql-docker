d24b6796dc8753e48e313aad608a3ced2570a566
saebom <saebom221@gmail.com>
2022-08-28 21:48:54 +0900

    08.28

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from icecream import ic
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 basedir = os.path.dirname(os.path.abspath(__file__))
 
@@ -7,21 +8,17 @@ from app.services.calculator import CalculatorService
 from app.services.user import UserService
 from app.services.score import ScoreService
 from app.services.grade import GradeService
+from app.services.pandas_quiz import PandasQuiz
 
 def print_menu():
     print("0. 전체프로그램 종료")
     print("1. 계산기 프로그램")
     print("2. 로그인 프로그램")     # 입력받은 아이디와 비번 콘솔에 출력하기
     print("3. 성적표 프로그램")     
+    print("4. 판다스 퀴즈 풀기")
     menu = input('메뉴 선택')
     return menu
 
-def score_menu():
-    print('0. 프로그램 종료')  
-    print('1. 성적 확인')  
-    menu = input('메뉴 선택')   
-    return menu 
-    
 def main():
     # print_menu()                            # expression(표현식) 메소드 -> 종류 2개
     # calculatorService = CalculatorService() # 수 (변수 -> 2개 : 전역/지역변수 -> self가 있는지 없는지)
@@ -55,7 +52,21 @@ def main():
             # grade = gradeService.get_grade(name, korean, english, math)   
             print(f'이름 : {name} 학점 : {score}')
             # print(f'이름 : {name} 학점 : {grade}')
-                
+            
+        elif menu == '4':
+            quiz = PandasQuiz()
+            while 1:
+                quiz_number = input('퀴즈번호 선택. 종료는 0 : ')
+                if quiz_number == '0':
+                    print('프로그램을 종료합니다.')
+                    break
+                elif quiz_number == '1':
+                    quiz.quiz1()                    
+                elif quiz_number == '2': 
+                    quiz.quiz2()   
+                elif quiz_number == '3': 
+                    quiz.quiz3()      
+                    
 if __name__ == '__main__':
     main()
     
